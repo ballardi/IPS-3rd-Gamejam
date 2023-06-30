e047509b87f7d4dce06632ae55e3d6e3db0f8880
Rohaid <rohaidb1999@gmail.com>
2023-06-30 19:06:03 -0400

    Added Unity Input System + Edited PlayerScript to respond / differentiate inputs.

--- Assets/Scripts/main/entity/PlayerScript.cs
+++ Assets/Scripts/main/entity/PlayerScript.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
+// author : Rohaid 
 public class PlayerScript : MonoBehaviour {
 
     public BoxCollider2D SuccessCollider;
@@ -14,15 +16,56 @@ public class PlayerScript : MonoBehaviour {
     private Collider2D[] failureColliderContactResults = new Collider2D[20];
     private Collider2D[] successColliderContactResults = new Collider2D[20];
 
+    // Input 
+    private PlayerInput input;
+
     void Awake() {
         Assert.IsNotNull(FailureCollider);
         Assert.IsNotNull(SuccessCollider);
+
+        input = new PlayerInput();
+        input.Enable();
+        input.Player.Enable();
+        input.Player.UP.Enable();
+        input.Player.DOWN.Enable();
+        input.Player.RIGHT.Enable();
+    }
+
+    void onEnable() {
+        input.Enable();
+        input.Player.Enable();
+        input.Player.UP.Enable();
+        input.Player.DOWN.Enable();
+        input.Player.RIGHT.Enable();
+    }
+
+    void OnDisable() {
+        input.Player.UP.Disable();
+        input.Player.DOWN.Disable();
+        input.Player.RIGHT.Disable();
+        input.Player.Disable();
+        input.Disable(); 
     }
 
     public void Update() {
-        // check for any success collisions
+      
+    }
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W)) {
+    void OnUP(){
+        Act();
+    }
+
+    void OnDOWN(){
+        Act();
+    }
+
+    void OnRIGHT(){
+        Act();
+    }
+
+    public void Act() {
+          // check for any success collisions
+            Debug.Log("Pressed Button");
             int countOfSuccessCollisions = SuccessCollider.OverlapCollider(contactFilter, successColliderContactResults);
             if (countOfSuccessCollisions > 0) {
                 for (int i = 0; i<countOfSuccessCollisions; i++) {
@@ -31,22 +74,21 @@ public class PlayerScript : MonoBehaviour {
                     successColliderContactResults[i].gameObject.GetComponentInChildren<ObstacleAScript>().changeState(ObstacleAScript.STATE.PlayerResolvedSuccessfully);
                 }
             }
-
-        }
-
-        // check for any failure collisions
-
-        int countOfFailureCollisions = FailureCollider.OverlapCollider(contactFilter, failureColliderContactResults);
-        if (countOfFailureCollisions > 0) {
-            for(int i = 0; i<countOfFailureCollisions; i++) {
-                Debug.Log($"player failure collision with: {failureColliderContactResults[i].transform.name}");
-                // TODO: handle collision: lose the game, etc.
-                this.GetComponentInChildren<SpriteRenderer>().color = Color.red;
-                failureColliderContactResults[i].gameObject.GetComponentInChildren<ObstacleAScript>().changeState(ObstacleAScript.STATE.PlayerFailed);
-            }
-        }
-    }
+    } 
 
 }
 
+// Checks that an object touched the back collider. Should be moved to an OnTrigger Enter script though instead
+// the update loop ?
 
+//  // check for any failure collisions
+
+//         int countOfFailureCollisions = FailureCollider.OverlapCollider(contactFilter, failureColliderContactResults);
+//         if (countOfFailureCollisions > 0) {
+//             for(int i = 0; i<countOfFailureCollisions; i++) {
+//                 Debug.Log($"player failure collision with: {failureColliderContactResults[i].transform.name}");
+//                 // TODO: handle collision: lose the game, etc.
+//                 this.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+//                 failureColliderContactResults[i].gameObject.GetComponentInChildren<ObstacleAScript>().changeState(ObstacleAScript.STATE.PlayerFailed);
+//             }
+//         }
