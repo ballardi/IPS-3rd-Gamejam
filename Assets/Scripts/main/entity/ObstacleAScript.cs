76817a95101170d1f280c383b43000d4d5b21dce
ballardi <1449350+ballardi@users.noreply.github.com>
2023-06-30 21:39:52 +1000

    skeleton architecture, POC input system, and placeholder toplevel prefabs

---
+++ Assets/Scripts/main/entity/ObstacleAScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAScript : MonoBehaviour {
+
+    public float startSpeed = 1;
+    private float currentSpeed;
+    private STATE currentState;
+
+    public enum STATE { Normal, PlayerResolvedSuccessfully, PlayerFailed };
+
+    void Awake() {
+        InitializeNewGame();
+    }
+
+    public void InitializeNewGame() {
+        currentSpeed = startSpeed;
+        currentState = STATE.Normal;
+    }
+
+    void Update() {
+        if(currentState == STATE.Normal || currentState == STATE.PlayerResolvedSuccessfully) {
+            // TODO: increase speed based on combination of deltaTime and some global game speed variable?
+            Vector3 currentPos = transform.localPosition;
+            transform.localPosition = new Vector3(currentPos.x - currentSpeed, currentPos.y, currentPos.z);
+        }
+    }
+
+    // TODO: depending on the new state, do things like...
+    // TODO: switch animation state (using animation trigger)
+    // TODO: enable/disable collider
+    // TODO: use Invoke(...) to disable the visual and movement after some time?
+    public void changeState(STATE newState) {
+
+        switch (newState) {
+            case STATE.PlayerResolvedSuccessfully:
+                gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green;
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                break;
+            // TODO: handle other state changes
+        }
+
+        currentState = newState;
+    }
+
+
+}
