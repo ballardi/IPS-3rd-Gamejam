07eeb24daae40023ee636e9a0b73f30d3782eb71
ballardi <1449350+ballardi@users.noreply.github.com>
2023-07-01 23:29:54 +1000

    Revert "Merge pull request #6 from ballardi/obstacle-tests"
    This reverts commit 6f1b269a52bd50150f34a663aaf52bd467cc0492, reversing
    changes made to cc3999c7473ba01d9309c0f7462b133d890df404.

--- Assets/Scripts/main/entity/Obstacle/ObstacleTimer.cs
+++ Assets/Scripts/main/entity/Obstacle/ObstacleTimer.cs
@@ -1,63 +1,96 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace ObstacleManagement
 {
-    /// <summary>
-    /// The ObstacleTimer class is a timer responsible for providing an obstacle generation interval.
-    /// Its timer duration is decreased each time an obstacle is generated until
-    /// the minimum duration has been reached.
-    /// </summary>
-    public class ObstacleTimer : Timer
+    public class ObstacleTimer
     {
         /// <summary>
         /// Determines the amount of seconds that the timer will wait initially.
-        /// This initial spawn rate duration will be decreased after each generated obstacle.
+        /// This initial spawn rate will be increased after each obstacle generation.
         /// </summary>
         public const float INITIAL_SPAWN_RATE = 1.2f;
 
         /// <summary>
-        /// Determines the amount of seconds that should be decreased from the timer duration
+        /// Determines the amount of seconds that should be decreased from the timer
         /// after each generated obstacle.
+        /// Since the timer will end sooner, this makes the game more difficult.
         /// The spawn rate should not be decremented if the minimum spawn rate has been reached.
         /// </summary>
         public const float SPAWN_RATE_DECREMENT = 0.03f;
 
         /// <summary>
         /// Determines the minimium amount of seconds that the timer will run.
-        /// After this rate has been reached, it cannot be decreased any more.
+        /// After this rate has been reached, it cannot be decremented any more.
         /// </summary>
         public const float MINIMUM_SPAWN_RATE = 0.3f;
 
         /// <summary>
-        /// Creates the ObstacleTimer, sets its spawn rate duration to the default spawn rate,
+        /// The current spawn rate determining the amount of seconds on the timer.
+        /// The spawn rate should be decreased after each generated obstacle.
+        /// </summary>
+        private float _spawnRate = INITIAL_SPAWN_RATE;
+
+        /// <summary>
+        /// The obstacle manager script, which is a MonoBehaviour.
+        /// This is used to:
+        /// - access its StartCoroutine method to push control to the manager
+        /// - alert it when the timer has ended
+        /// </summary>
+        private readonly ObstacleManager _parent;
+
+        /// <summary>
+        /// Used to track if there is a timer running at the moment.
+        /// If a timer is running, the system should not try to start it.
+        /// </summary>
+        private Coroutine _clock;
+
+        /// <summary>
+        /// Creates the ObstacleTimer, sets its spawn rate to the default spawn rate,
         /// and uses the specified obstacle manager script as the parent.
         /// </summary>
         /// <param name="parent">The non-null reference to the ObstacleManager script</param>
         public ObstacleTimer(ObstacleManager parent)
-            : base(parent)
         {
-            _duration = INITIAL_SPAWN_RATE;
+            Assert.IsNotNull(parent);
+            _parent = parent;
         }
 
         /// <summary>
-        /// Decreases the amount of seconds on the timer.
+        /// Starts the timer if it is not already running.
+        /// Uses the current spawn rate as the duration of the timer in seconds.
+        /// If the timer is running, an Exception is thrown.
+        /// </summary>
+        public void Start()
+        {
+            Assert.IsNull(_clock, "Should not start a timer if it is already running!");
+            _clock = _parent.StartCoroutine(Tick());
+        }
+
+        /// <summary>
+        /// Decreases the amount of seconds on the timer, therefore increasing the spawn rate.
         /// If this would be less than the minimum spawn rate, the spawn rate will be set to the minimum.
         /// </summary>
         public void IncreaseSpawnRate()
         {
-            _duration = Mathf.Clamp(
-                _duration - SPAWN_RATE_DECREMENT,
+            _spawnRate = Mathf.Clamp(
+                _spawnRate - SPAWN_RATE_DECREMENT,
                 MINIMUM_SPAWN_RATE,
                 INITIAL_SPAWN_RATE
             );
         }
 
         /// <summary>
-        /// When the timer has ended, it notifies the ObstacleManager.
+        /// Waits until the amount of seconds of the current spawn rate have passed,
+        /// stops the timer, and then notifies the ObstacleManager that the timer has ended.
         /// </summary>
-        protected override void Alert()
+        /// <returns>An IEnumerator in order to wait for the spawn rate to end</returns>
+        private IEnumerator Tick()
         {
-            (_parent as ObstacleManager).NotifyOfObstacleTimerEnd();
+            yield return new WaitForSeconds(_spawnRate);
+            _clock = null;
+            _parent.NotifyOfObstacleTimerEnd();
         }
     }
 }
