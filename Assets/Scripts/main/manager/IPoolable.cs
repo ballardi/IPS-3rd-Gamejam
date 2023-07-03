9ef1144c446ccf05fa0e4c0139451b3737aaf371
ballardi <1449350+ballardi@users.noreply.github.com>
2023-07-03 23:59:15 +1000

    add some comments

--- Assets/Scripts/main/manager/IPoolable.cs
+++ Assets/Scripts/main/manager/IPoolable.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public interface IPoolable {
 
+    // called by ObjectPool when an object is taken out of the pool to be used
     public void InitializeOnUse();
 
+    // called by ObjectPool when an object is being put back into the pool
     public void DeInitializeOnPooling();
 
 }
