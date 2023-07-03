c1b98e2a143cef2b4bca405c57b623f04895f253
Gingaabread <none>
2023-07-03 22:00:05 +0200

    Animation

--- Assets/Scripts/main/view/Scoreboard/ScoreboardView.cs
+++ Assets/Scripts/main/view/Scoreboard/ScoreboardView.cs
@@ -7,9 +7,6 @@ public class ScoreboardView : MonoBehaviour
     [SerializeField]
     private TMP_Text _scoreText;
 
-    [SerializeField]
-    private Slider _scoreMetre;
-
     private bool _shouldDisplay;
     private int _currentScore;
     private int _destinationScore;
@@ -23,11 +20,10 @@ public class ScoreboardView : MonoBehaviour
             if (_currentTimer == 0)
             {
                 _currentTimer = 5;
-                _scoreMetre.value = _currentScore;
                 _scoreText.text = $"${_currentScore}";
                 _currentScore++;
 
-                if (_currentScore >= _destinationScore)
+                if (_currentScore > _destinationScore)
                     OnFinish();
             }
             else
@@ -40,7 +36,6 @@ public class ScoreboardView : MonoBehaviour
         _currentScore = 0;
         _currentTimer = 5;
         _destinationScore = GameStateManager.instance.CurrentScore;
-        _scoreMetre.value = 0;
 
         _shouldDisplay = true;
     }
